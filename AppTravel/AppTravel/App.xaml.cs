7daf2650dea874fcd61cd3b19e13f49c9319f72e
ly0n3l <tambalyo@gmail.com>
2021-02-23 05:33:29 +0100

    Ajoutez des fichiers projet.

---
+++ AppTravel/AppTravel/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppTravel
+{
+    public partial class App : Application
+    {
+        public string Databaselocation = string.Empty;
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+        
+        public App(string databaselocation)
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+
+            Databaselocation = databaselocation;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ AppTravel/AppTravel/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppTravel
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void LoginButton_Clicked(object sender, System.EventArgs e)
+        {
+            bool isEmail = string.IsNullOrEmpty(email.Text);
+            bool isPassword = string.IsNullOrEmpty(password.Text);
+
+            if(isEmail || isPassword)
+            {
+
+            }
+            else
+            {
+                
+            }
+
+            Navigation.PushAsync(new HomePage());
+           
+
+        }
+
+    }
+}
